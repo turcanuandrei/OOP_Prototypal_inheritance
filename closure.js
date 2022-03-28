174cd9f259f015b1450a5a6f995ad2d08648d23a
Andrei Turcanu <87308199+turcanuandrei@users.noreply.github.com>
2022-03-28 18:54:56 +0300

    Create closure.js
    I left a comment at the end of the file and an alternative way of doing it. But your way is 100% corect. Good job

--- closure.js
+++ closure.js
@@ -27,3 +27,36 @@
 // console.log(group.subtract(2));
 // console.log(group.multiply(2));
 // console.log(group.derive(2));
+
+/* good but you could've returned the object right away with sum, subtract and so on as keys */
+function getMathModule(z) {
+  let y = z;
+  let obj = {};
+  obj.sum = function (x) {
+    return y + x;
+  };
+  obj.subtract = function (x) {
+    return y - x;
+  };
+  obj.derive = function (x) {
+    return y / x;
+  };
+  obj.multiply = function (x) {
+    return y * x;
+  };
+  return {
+    sum(x) {
+      return y + x;
+    },
+    subtract(x) {
+      return y - x;
+    },
+    derive(x) {
+      return y / x;
+    },
+    multiply(x) {
+      return y * x;
+    },
+  };
+}
+
